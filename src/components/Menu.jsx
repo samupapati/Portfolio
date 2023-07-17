74e3be7ddcd8ba538ce9693e11f3133b4f104588
samupapati <samuelpapati@gmail.com>
2023-07-17 19:13:44 -0300

    Função para fechar o menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,23 +11,26 @@ function Menu(){
             checkbox.checked = false
         }
     }
+    function redirectLinkAndCloseMenu(redirect){
+        let checkbox = document.getElementById('menu-check')
+        if(checkbox.checked){
+            checkbox.checked = false;
+        }
+        window.scrollTo(document.getElementById(document.getElementById(redirect)))
+    }
 
     return (
         <header id="menu" className='container'>
             <div id='menu-items'>
                 <input type="checkbox" id='menu-check'/>
-                <nav id='links-social' className='menu-links'>
-                    <ul className='menu-links-list list'>
-                        <SocialNetworks/>
-                    </ul>
-                </nav>
-                <nav id='links-page' className='menu-links'>
-                    <ul className='menu-links-list list'>
-                        <li className='menu-links-list-item'><a href='#' className='link link-text'>Home</a></li>
-                        <li className='menu-links-list-item'><a href='#sobre' className='link link-text'>Sobre</a></li>
-                        <li className='menu-links-list-item'><a href='#' className='link link-text'>Projetos</a></li>
-                    </ul>    
-                </nav>
+                <ul id='links-social' className='menu-links'>
+                    <SocialNetworks/>
+                </ul>
+                <ul id='links-page' className='menu-links'>
+                    <li className='menu-links-list-item'><a href='#home' className='link link-text' onClick={() => redirectLinkAndCloseMenu('home')}>Home</a></li>
+                    <li className='menu-links-list-item'><a href='#certifications' className='link link-text' onClick={() => redirectLinkAndCloseMenu('certifications')}>Certificações</a></li>
+                    <li className='menu-links-list-item'><a href='#projects' className='link link-text' onClick={() => redirectLinkAndCloseMenu('projects')}>Projetos</a></li>
+                </ul>
                 <Toggle/>
                 <label htmlFor="menu-check" id='menu-btn-hamburguer'><AiOutlineMenu/></label>
             </div>
