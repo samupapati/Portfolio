7fb580a69ebeff07edfe1828791503c4aa0a00d4
samupapati <samuelpapati@gmail.com>
2023-07-17 19:11:29 -0300

    add botao figma

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,14 @@
-import '../assets/styles/buttons.css'
-
 function Buttons({links}){
     return(
         <>
             <li className="button"><a href={links[0]} target='_blank' rel='noreferrer' className="button-link">Ver projeto</a></li>
             <li className="button"><a href={links[1]} target='_blank' rel='noreferrer' className="button-link">Ver código</a></li>
+            {
+            links.length === 3 ? 
+                <li className="button"><a href={links[2]} target='_blank' rel='noreferrer' className="button-link">Ver figma</a></li>
+            :
+                ""
+            }
         </>
     )
 }
